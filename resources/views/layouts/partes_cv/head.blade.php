e00a296e66ea86125f637f5bbaf256e7949f5802
Pedro Jesús Bazó Canelón <bazo.pedro@gmail.com>
2021-08-04 21:41:21 -0400

    Integración del CV anterior prueba 12

--- resources/views/layouts/partes_cv/head.blade.php
+++ resources/views/layouts/partes_cv/head.blade.php
@@ -26,5 +26,7 @@
 <link href="//fonts.googleapis.com/css?family=Nunito:400,600,700,800,900&display=swap" rel="stylesheet">
 <link href="//fonts.googleapis.com/css?family=Hind&display=swap" rel="stylesheet">
 <!-- Template CSS -->
-<link rel="stylesheet" href="{{ asset('assets/css/style-starter.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/mis_estilos.css') }}">
+{{-- <link rel="stylesheet" href="{{ asset('assets/css/style-starter.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/mis_estilos.css') }}"> --}}
+<link rel="stylesheet" href="assets/css/style-starter.css">
+<link rel="stylesheet" href="assets/css/mis_estilos.css">
